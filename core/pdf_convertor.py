21d447df6320b29f61f9dc7d6edfef7a7ba94ae2
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-07 20:12:18 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -12,18 +12,14 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        try:
-            subprocess.call(['soffice',
+        subprocess.call(['soffice',
                          '--headless',
                          '--convert-to',
                          'pdf',
                          '--outdir',
                          FILE_FOLDER,
                          self.file])
-            delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
-            os.remove(delete_path)
-            path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
-            return path
-        except:
-            self.error = ErrorType.no_correct_doc
-            return self.error
+        delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
+        os.remove(delete_path)
+        path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
+        return path
