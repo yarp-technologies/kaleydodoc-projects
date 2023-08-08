951c514e824e6275df5dc0d67909c4d06fa4a14c
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-08 23:21:30 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -13,12 +13,24 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        subprocess.run(["unoconv", "-f", "pdf", "-o",
-            FILE_FOLDER,
-            self.file])
+        url = "http://localhost:3000/unoconv/pdf"
+        files = {"file": open(self.file, "rb")}
+        response = requests.post(url, files=files)
+
+        if response.status_code == 200:
+            pdf_path = os.path.join(FILE_FOLDER, os.path.basename(self.file).split('.')[0] + ".pdf")
+            with open(pdf_path, "wb") as pdf_file:
+                pdf_file.write(response.content)
+
+        '''subprocess.call(['soffice',
+                         '--headless',
+                         '--convert-to',
+                         'pdf',
+                         '--outdir',
+                         FILE_FOLDER,
+                         self.file])
+        '''
         delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
         path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
         return path
-
-# Convert2PDF("../test_files/typical.docx").DocxToPdf()
