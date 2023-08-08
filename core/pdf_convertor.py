4a726354baa56854f76bc680a6ec67669674c8d7
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-08 22:25:03 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -13,42 +13,18 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        # Определите URL и порт LibreOffice Online сервиса
-        libreoffice_online_url = "http://soffice:9980"  # Имя сервиса из вашего docker-compose.yml
-
-        # Отправьте запрос на конвертацию
-        response = requests.post(
-            f"{libreoffice_online_url}/lool/convert-to/pdf",
-            data=open(self.file, "rb").read(),
-            headers={"content-type": "application/octet-stream"}
-        )
-
-        if response.status_code == 200:
-            pdf_path = os.path.join(FILE_FOLDER, f"{os.path.basename(self.file).split('.')[0]}.pdf")
-            with open(pdf_path, "wb") as pdf_file:
-                pdf_file.write(response.content)
-
-            delete_path = os.path.join(FILE_FOLDER, os.path.basename(self.file))
-            os.remove(delete_path)
-
-            return pdf_path
-        else:
-            print(f"Ошибка при конвертации: {response.status_code}")
-            return None
-
-'''
-    def DocxToPdf(self):
-        subprocess.call(['soffice',
-                         '--headless',
-                         '--convert-to',
-                         'pdf',
-                         '--outdir',
-                         FILE_FOLDER,
-                         self.file])
+        subprocess.Popen([
+            '/usr/bin/soffice',
+            '--headless',
+            '--convert-to',
+            '--convert-to',
+            'pdf',
+            '--outdir',
+            FILE_FOLDER,
+            self.file])
         delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
         path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
         return path
-'''
 
-
+# Convert2PDF("../test_files/typical.docx").DocxToPdf()
