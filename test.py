d7c97bd8883061620f39454c575e04c4f4ee1a2f
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-09 19:21:07 +0500

    server

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
-from docx import Document
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+import subprocess
+import os
+import requests
+from constants.variables import *
+from constants.msg import ErrorType
 
 
-def convert_docx_to_pdf(docx_file_path, pdf_file_path):
-    # Открываем DOCX файл
-    doc = Document(docx_file_path)
+class Convert2PDF:
 
-    # Создаем PDF файл с помощью reportlab
-    c = canvas.Canvas(pdf_file_path, pagesize=letter)
+    def __init__(self, file_path: str):
+        # Todo под расширение для других форматов
+        self.file = file_path
+        self.error = ErrorType.ok
 
-    for para in doc.paragraphs:
-        c.drawString(100, 700, para.text)
-        c.showPage()
-
-    c.save()
-
-
-# Укажите пути к вашим файлам
-docx_path = "test_files/typical.docx"
-pdf_path = "output.pdf"
-
-convert_docx_to_pdf(docx_path, pdf_path)
+    def DocxToPdf(self):
+        subprocess.run(["docker-compose", "exec", "libreoffice", "libreoffice",
+                        "--headless", "--convert-to", "pdf", "--outdir",
+                         FILE_FOLDER,
+                         self.file])
+        delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
+        os.remove(delete_path)
+        path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
+        return path
