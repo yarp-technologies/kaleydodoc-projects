a2aa3358825d450920d62b8802cbc061e0e444e3
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-10 04:25:36 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -13,20 +13,25 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        try:
-            client = docker.from_env()
-            libreoffice_container = client.containers.get("pdf_placeholder-libreoffice-1")
-            cmd = f'soffice ' \
-                  f'--headless ' \
-                  f'--convert-to pdf ' \
-                  '"-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}" ' \
-                  f'--outdir ' \
-                  f'/project/files ' \
-                  f'{self.file.replace("..", "/project")}'
-            result = libreoffice_container.exec_run(cmd)
-            delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
-            os.remove(delete_path)
-            pdf_path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
-            return pdf_path.replace("..", "project")
-        except:
-            return ErrorType.no_correct_doc
+        '''
+        subprocess.call(['soffice',
+                         '--headless',
+                         '--convert-to',
+                         'pdf',
+                         '--outdir',
+                         FILE_FOLDER,
+                         self.file])
+        delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
+        os.remove(delete_path)
+        path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
+        return path
+        :return:
+        '''
+        client = docker.from_env()
+        libreoffice_container = client.containers.get("pdf_placeholder-libreoffice-1")
+        cmd = f"libreoffice --headless --convert-to pdf --outdir {FILE_FOLDER} {self.file}"
+        result = libreoffice_container.exec_run(cmd)
+        delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
+        os.remove(delete_path)
+        pdf_path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
+        return pdf_path
