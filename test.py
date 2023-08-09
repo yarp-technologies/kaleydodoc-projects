83bcb52f235efc0f1556a9e5488dcf1cc3685462
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-09 21:38:00 +0500

    server

--- test.py
+++ test.py
@@ -17,11 +17,11 @@ class Convert2PDF:
         print(1)
         libreoffice_container = client.containers.get("pdf_placeholder-libreoffice-1")
         print(2)
-        cmd = f"libreoffice --headless --convert-to pdf --outdir {FILE_FOLDER} {self.file}"
+        cmd = f"libreoffice --headless --convert-to pdf --outdir '/file' {self.file}"
         print(3)
         result = libreoffice_container.exec_run(cmd)
         print(4)
-        delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
+        delete_path = "file" + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
         pdf_path = f"/files/{os.path.splitext(self.file)[0]}.pdf"
         return pdf_path
