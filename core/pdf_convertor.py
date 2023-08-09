92fe46514b84db4c20a484d1a10724b2461c6786
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-09 17:09:14 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -13,14 +13,11 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        subprocess.run(["docker",
-                        "exec",
-                        "libreoffice",
-                        "libreoffice",
+        subprocess.run(["libreoffice",
                         "--convert-to", "pdf",
                         "--outdir",
-                         FILE_FOLDER,
-                         self.file])
+                        FILE_FOLDER,
+                        self.file])
         delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
         path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
