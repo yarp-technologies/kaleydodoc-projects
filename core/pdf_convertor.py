18fa73eeb2d2bb7fa9c08944f48a1156e441716e
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-09 06:13:38 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -13,23 +13,14 @@ class Convert2PDF:
         self.error = ErrorType.ok
 
     def DocxToPdf(self):
-        url = "http://localhost:3000/unoconv/pdf"
-        files = {"file": open(self.file, "rb")}
-        response = requests.post(url, files=files)
-
-        if response.status_code == 200:
-            pdf_path = os.path.join(FILE_FOLDER, os.path.basename(self.file).split('.')[0] + ".pdf")
-            with open(pdf_path, "wb") as pdf_file:
-                pdf_file.write(response.content)
-
-        '''subprocess.call(['soffice',
-                         '--headless',
-                         '--convert-to',
-                         'pdf',
-                         '--outdir',
+        subprocess.run(["docker",
+                        "exec",
+                        "libreoffice",
+                        "libreoffice",
+                        "--convert-to", "pdf",
+                        "--outdir",
                          FILE_FOLDER,
                          self.file])
-        '''
         delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
         path = FILE_FOLDER + '/' + self.file.split('/')[-1].split('.')[0] + '.pdf'
