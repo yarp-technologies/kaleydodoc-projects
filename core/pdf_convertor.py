450a5fc44edc1683a42c800f5d769340952c5c43
solomonalfred <nik_bogdanov2002@mail.ru>
2023-08-10 02:44:58 +0500

    server

--- core/pdf_convertor.py
+++ core/pdf_convertor.py
@@ -29,7 +29,13 @@ class Convert2PDF:
         '''
         client = docker.from_env()
         libreoffice_container = client.containers.get("pdf_placeholder-libreoffice-1")
-        cmd = f"libreoffice --headless --convert-to pdf --outdir {FILE_FOLDER} {self.file}"
+        cmd = f'soffice ' \
+              f'--headless ' \
+              f'--convert-to pdf:writer_pdf_Export ' \
+              f'--outdir ' \
+              '"-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}"' \
+              f'{FILE_FOLDER} ' \
+              f'{self.file}'
         result = libreoffice_container.exec_run(cmd)
         delete_path = FILE_FOLDER + '/' + self.file.split('/')[-1]
         os.remove(delete_path)
